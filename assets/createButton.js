1f7db7d65f11b83670c642afcec9d6862ab3002c
Mau Alanes <alanesmauricio@gmail.com>
2022-04-14 09:08:51 -0400

    js_terminado

--- assets/createButton.js
+++
@@ -1,9 +0,0 @@
-export function createButton (content, id){
-    const button = document.createElement('button')
-    button.textContent = content
-    button.className = "button"
-    button.id = id
-    
-    const mountButton = document.querySelector(".conte-butt")
-    mountButton.appendChild(button) 
-}
